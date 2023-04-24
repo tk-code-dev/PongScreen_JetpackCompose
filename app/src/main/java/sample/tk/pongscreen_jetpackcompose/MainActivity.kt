6578e5821c63b155d24f7d7d403f00ca2c36d4e4
tk-code-dev <reiwa.dupuis.2019@gmail.com>
2023-04-23 23:06:17 -0400

    draw circle

--- app/src/main/java/sample/tk/pongscreen_jetpackcompose/MainActivity.kt
+++ app/src/main/java/sample/tk/pongscreen_jetpackcompose/MainActivity.kt
@@ -1,46 +1,75 @@
 package sample.tk.pongscreen_jetpackcompose
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.offset
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import sample.tk.pongscreen_jetpackcompose.ui.theme.PongScreen_JetpackComposeTheme
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PongScreen_JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            PongGame()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun PongGame() {
+    var ballPosition by remember { mutableStateOf(Offset(0f, 0f)) }
+    var ballVelocity by remember { mutableStateOf(Offset(10f, 10f)) }
+    val ballRadius = 50f.dp
+    val screenWidth = 720f.dp
+    val screenHeight = 1280f.dp
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PongScreen_JetpackComposeTheme {
-        Greeting("Android")
+    LaunchedEffect(Unit) {
+        while (true) {
+            ballPosition += ballVelocity
+            if (ballPosition.x - ballRadius.value < 0 || ballPosition.x + ballRadius.value > screenWidth.value) {
+                ballVelocity = Offset(-ballVelocity.x, ballVelocity.y)
+            }
+            if (ballPosition.y - ballRadius.value < 0 || ballPosition.y + ballRadius.value > screenHeight.value) {
+                ballVelocity = Offset(ballVelocity.x, -ballVelocity.y)
+            }
+            delay(16)
+        }
+    }
+
+    val ballColor = animateFloatAsState(
+        targetValue = if (ballVelocity.x > 0) 1f else 0f,
+        animationSpec = tween(durationMillis = 1000)
+    ).value
+
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .offset(
+                x = (ballPosition.x - ballRadius.value).dp,
+                y = (ballPosition.y - ballRadius.value).dp
+            )
+    ) {
+        drawCircle(
+            color = Color(
+                red = ballColor,
+                green = 0f,
+                blue = 1 - ballColor,
+                alpha = 1f
+            ),
+            radius = ballRadius.value,
+            style = Stroke(5f)
+        )
     }
 }
